684b457ea44dbe518171cb3639e9c426f5591da9
Herman <zhanghm_1995@qq.com>
2022-07-21 20:45:20 +0800

    load the HDTF face dataset ok and could train in original image :tada:

--- scripts/train_512p_HDTF.sh
+++ scripts/train_512p_HDTF.sh
@@ -2,4 +2,4 @@ set -x
 
 python train.py --name HDTF_512p --dataset_name FaceDataset --dataroot ./datasets/HDTF_preprocessed \
                 --flist ./datasets/train.txt --batchSize 2 \
-                --no_instance --label_nc 0
+                --no_instance --label_nc 0 --resize_or_crop none
